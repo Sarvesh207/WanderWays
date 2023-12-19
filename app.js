435e96c32be3dc626aca5a34e8754ff0f74f119a
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-19 23:37:18 +0530

    wander ways

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const Listing = require('./models/listing');
-
-
-
 const mongoose = require('mongoose');
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderways'
+const path = require('path');
+const methodOverride = require('method-override');
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 main()
 .then(() => {console.log("connected to db")})
@@ -18,20 +21,68 @@ async function main(){
 }
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get("/testListing", async (req, res) => {
-    let smapleListing  = new Listing({
-        title:"My New Villa",
-        description:"By the Beach",
-        price:1200,
-        location:"Calangute, Goa",
-        country:"India",
 
-    });
-   await smapleListing.save();
-   console.log("Sample was Save");
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-   res.send("Sample was  succeuufully");
+// app.get("/testListing", async (req, res) => {
+//     let smapleListing  = new Listing({
+//         title:"My New Villa",
+//         description:"By the Beach",
+//         price:1200,
+//         location:"Calangute, Goa",
+//         country:"India",
+
+//     });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Index route
+app.get("/listings", async (req, res) => {
+    const allListings =  await Listing.find({});
+
+    res.render("listings/index.ejs", {allListings});
 })
+
+// New route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+})
+
+// Create Route
+app.post("/listings", async (req, res) => {
+    
+    const newListing = await new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+
+})
+
+
+
+
+
+
 app.listen(3000, () => console.log('listening on port 3000'))
--- models/listing.js
+++ models/listing.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title:{
         type:String,
-        required:true,
+        required:true,  
     },
     
     description:String,
     image:{
-        type:Object,
+        type:String,
         required:true,
         default:"https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwbmF0dXJlfGVufDB8fDB8fHww",
     },
